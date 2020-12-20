6d88ed2a59191871f913efdf0e1f601e56df60a3
TypicalIllusion <70919038+TypicalIllusion@users.noreply.github.com>
2020-12-19 19:55:33 -0500

    Update AutoNuke.cs

--- AutoNuke/AutoNuke.cs
+++ AutoNuke/AutoNuke.cs
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using MEC;
-=======
->>>>>>> 7b7ea67580596c0d9724639920c343d9a64ca741
 using Synapse.Api.Plugin;
 
 namespace AutoNuke
@@ -15,18 +12,11 @@ namespace AutoNuke
     SynapseMajor = 2,
     SynapseMinor = 3,
     SynapsePatch = 0,
-<<<<<<< HEAD
     Version = "1.2.0"
 )]
     class AutoNuke : AbstractPlugin
     {
         public static List<CoroutineHandle> Coroutine = new List<CoroutineHandle>();
-=======
-    Version = "1.1.0"
-)]
-    class AutoNuke : AbstractPlugin
-    {
->>>>>>> 7b7ea67580596c0d9724639920c343d9a64ca741
         [Synapse.Api.Plugin.Config(section = "AutoNuke")]
         public static Config Config;
         public override void Load()
